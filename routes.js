eceffcb09d4454002d709e29892949c77100b3a9
lyc242 <43083530+lyc242@users.noreply.github.com>
2019-07-29 17:33:52 +0800

    init

---
+++ routes.js
@@ -0,0 +1,116 @@
+'use strict';
+
+const express = require('express');
+const { check, validationResult } = require('express-validator');
+const bcryptjs = require('bcryptjs');
+const auth = require('basic-auth');
+
+// This array is used to keep track of user records
+// as they are created.
+const users = [];
+
+const authenticateUser = (req, res, next) => {
+	let message = null;
+
+	// Parse the user's credentials from the Authorization header.
+	const credentials = auth(req);
+
+	// If the user's credentials are available...
+	if (credentials) {
+		// Attempt to retrieve the user from the data store
+		// by their username (i.e. the user's "key"
+		// from the Authorization header).
+		const user = users.find(u => u.username === credentials.name);
+
+		// If a user was successfully retrieved from the data store...
+		if (user) {
+			// Use the bcryptjs npm package to compare the user's password
+			// (from the Authorization header) to the user's password
+			// that was retrieved from the data store.
+			const authenticated = bcryptjs.compareSync(
+				credentials.pass,
+				user.password
+			);
+			// If the passwords match...
+			if (authenticated) {
+				// Then store the retrieved user object on the request object
+				// so any middleware functions that follow this middleware function
+				// will have access to the user's information.
+				req.currentUser = user;
+			} else {
+				message = `Authentication failure for username: ${user.username}`;
+			}
+		} else {
+			message = `User not found for username: ${credentials.name}`;
+		}
+	} else {
+		message = 'Auth header not found';
+	}
+
+	// If user authentication failed...
+	if (message) {
+		console.warn(message);
+
+		// Return a response with a 401 Unauthorized HTTP status code.
+		res.status(401).json({ message: 'Access Denied' });
+	} else {
+		// Or if user authentication succeeded...
+		// Call the next() method.
+		next();
+	}
+};
+
+// Construct a router instance.
+const router = express.Router();
+
+// Route that returns the current authenticated user.
+router.get('/users', authenticateUser, (req, res) => {
+	const user = req.currentUser;
+
+	res.json({
+		name: user.name,
+		username: user.username
+	});
+});
+
+// Route that creates a new user.
+router.post(
+	'/users',
+	[
+		check('name')
+			.exists({ checkNull: true, checkFalsy: true })
+			.withMessage('Please provide a value for "name"'),
+		check('username')
+			.exists({ checkNull: true, checkFalsy: true })
+			.withMessage('Please provide a value for "username"'),
+		check('password')
+			.exists({ checkNull: true, checkFalsy: true })
+			.withMessage('Please provide a value for "password"')
+	],
+	(req, res) => {
+		// Attempt to get the validation result from the Request object.
+		const errors = validationResult(req);
+
+		// If there are validation errors...
+		if (!errors.isEmpty()) {
+			// Use the Array `map()` method to get a list of error messages.
+			const errorMessages = errors.array().map(error => error.msg);
+
+			// Return the validation errors to the client.
+			return res.status(400).json({ errors: errorMessages });
+		}
+		// Get the user from the request body.
+		const user = req.body;
+
+		// Hash the new user's password.
+		user.password = bcryptjs.hashSync(user.password);
+
+		// Add the user to the `users` array.
+		users.push(user);
+
+		// Set the status to 201 Created and end the response.
+		return res.status(201).end();
+	}
+);
+
+module.exports = router;
